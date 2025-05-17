26a1b5f933c655a15d66da9666d679fa39a24b19
SurfingOwl <arthur.cesar.fr@gmail.com>
2025-05-17 22:17:41 +0200

    debug

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,6 @@ import NextAuth from "next-auth";
 import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
+console.log("NEXTAUTH_URL at runtime:", process.env.NEXTAUTH_URL);
 
 export { handler as GET, handler as POST };
